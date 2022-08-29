2ea85d68d2c46c2f9cb50c81e02fe7d9bc999a2f
neonuser1414 <constant8765@gmail.com>
2022-08-29 18:11:17 +0300

    Change text blocks.
    In this version:
    
    1. Improving the text design of the program
    2. Changing the content of text blocks

--- Quiz.py
+++ Quiz.py
@@ -1,23 +1,23 @@
 #The program demonstrates the operation of if else statements
-number = input("Welcome to the quiz program! \n Press 1 - to continue \nPress 2 - to close the program  \n ")
+number = input("Welcome to the quiz program! \n Press 1 - to continue \n Press 2 - to close the program  \n Choise: ")
 #For example, if the number 1 is entered, then the quiz will start
 if number == "1":
-        print("Great! Let's get started!")
-        variant = input("In what year was FIFA founded? \n 1 - 1904 year \n 2 - 1906 year \n 3 - 1920 year: \n ")
+        print("Ok! Let's get started! \n First question:")
+        variant = input("In what year was FIFA founded? \n 1 - 1904 year \n 2 - 1906 year \n 3 - 1920 year: \n Answer: ")
         if variant == "1":
                 #Here you guessed it
-            print("You guessed it!")
-            variant = input("What was the first processor released by Intel? \n 1 - Intel Atom \n 2 - Intel 4004 \n 3 - Intel Pentium: \n ")
+            print("You guessed it! \n Second question:")
+            variant = input("What was the first processor released by Intel? \n 1 - Intel Atom \n 2 - Intel 4004 \n 3 - Intel Pentium: \n Answer: ")
             if variant == "2":
-                print("Great! You guessed it again! Let's move on!")
-                variant = input("Date of appearance of the Python programming language \n 1 - 1991 year \n 2 - 1996 year \n 3 - 2001 year: \n ")
+                print("Great! You guessed it again! Let's move on! \n Third question:")
+                variant = input("Date of appearance of the Python programming language \n 1 - 1991 year \n 2 - 1996 year \n 3 - 2001 year: \n Answer: ")
                 if variant == "1":
-                    print("Excellent! You have good knowledge! And now the last question!")
-                    variant = input("When did Windows 10 come out? \n 1 - 2 september 2015 year \n 2 - 30 june 2015 year \n 3 - 29 july 2015 year: \n ")
+                    print("Excellent! You have good knowledge! And now the last - fourth question!: ")
+                    variant = input("When did Windows 10 come out? \n 1 - 2 september 2015 year \n 2 - 30 june 2015 year \n 3 - 29 july 2015 year: \n Answer: ")
                     if variant == "3":
                             #The program congratulates you and now you can press Enter to exit the program
                         print("Your knowledge is amazing! Thank you for your participation! See you soon!")
-                        input("To confirm the exit, press Enter")
+                        input("To confirm the exit, press Enter \n Confirmation: ")
                     else:
                             #It looks like you made a mistake
                         print("Oops... It seems you made a mistake. Try again!")
